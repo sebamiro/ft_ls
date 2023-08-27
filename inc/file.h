03e47e51c9d2152e22995db7959848da86f9e03a
sebamiro <seba.mi.21@gmail.com>
2023-08-27 23:28:41 +0200

    Added free_files function

--- inc/file.h
+++ inc/file.h
@@ -13,5 +13,6 @@ typedef struct s_file
 t_file* new_node(struct dirent *rdir);
 t_file* add_lexicographical(t_file **head, struct dirent *rdir);
 t_file* last_node(t_file **head);
+void free_files(t_file *head);
 
 #endif
