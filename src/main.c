c5047f620c4720cc0b3fb1a3c171c23d9cb062a2
sebamiro <seba.mi.21@gmail.com>
2023-09-10 23:23:54 +0200

    Blazing fast ls

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/dirent.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
@@ -13,14 +14,64 @@ enum modes {
     TIME = 0x10
 };
 
+enum file_type {
+    unknown,
+    blockdev,
+    chardev,
+    directory,
+    fifo,
+    symbolic_link,
+    normal,
+    sock
+};
+
 typedef struct s_pending
 {
     char *name;
     struct s_pending *next;
 } t_pending;
 
+typedef struct s_fileinfo
+{
+    char *name;
+    enum file_type type;
+} t_fileinfo;
+
 static char mode = 0;
 t_pending *pending = NULL;
+t_fileinfo *cwd;
+void **sorted;
+size_t cwd_n;
+size_t cwd_alloc;
+size_t sorted_files_alloc;
+
+static void
+clear_file(void)
+{
+    size_t i;
+
+    for (i = 0; i < cwd_n; i++) {
+        t_fileinfo *f = sorted[i];
+        free(f->name);
+    }
+    cwd_n = 0;
+}
+
+static char*
+file_name_concat(const char *dirname, const char *name)
+{
+    size_t total;
+    char *cat; 
+
+    total = strlen(dirname) + strlen(name) + 2;
+    cat = calloc(total, 1);
+    if (!cat)
+        exit(3);
+    strlcat(cat, dirname, total);
+    strlcat(cat, "/", total);
+    strlcat(cat, name, total);
+    return cat;
+}
 
 static void
 queue_directory(const char *name)
@@ -35,6 +86,103 @@ queue_directory(const char *name)
     pending = new;
 }
 
+static void
+initialize_sort(void)
+{
+    size_t i;
+
+    for (i = 0; i < cwd_n; i++) {
+        sorted[i] = &cwd[i];
+    }
+}
+
+static void
+sort_files(void)
+{
+    if (sorted_files_alloc < cwd_n + cwd_n / 2)
+    {
+        free(sorted);
+        sorted = malloc(cwd_n * 3 * sizeof *sorted);
+        sorted_files_alloc = 3 * cwd_n;
+    }
+    initialize_sort();
+
+    size_t n = cwd_n;
+    while (n > 1) {
+        size_t newn = 0;
+        for (size_t i = 1; i <= n - 1; i++) {
+            if (strcmp(
+                        ((t_fileinfo **)sorted)[i - 1]->name,
+                        ((t_fileinfo **)sorted)[i]->name) > 0
+                    ) {
+               void *t = sorted[i - 1]; 
+               sorted[i - 1] = sorted[i];
+               sorted[i] = t;
+               newn = i;
+            }
+        }
+        n = newn;
+    }
+}
+
+static void
+register_file(const char *name, enum file_type type)
+{
+    t_fileinfo *f;
+
+    if (cwd_n == cwd_alloc) {
+        cwd = realloc(cwd, cwd_alloc * 2 * sizeof *cwd);
+        cwd_alloc *= 2;
+    }
+    f = &cwd[cwd_n];
+    bzero(f, sizeof *f);
+    f->type = type;
+    f->name = strdup(name);
+    if (!f->name)
+        exit(3);
+    cwd_n++;
+}
+
+static bool
+dot_or_dotdot(const char *file_name)
+{
+    if (file_name[0] == '.')
+        return !file_name[(file_name[1] == '.') + 1];
+    return false;
+}
+
+static void
+extract_dir_from_files(const char *dir_name)
+{
+    size_t i;
+
+    for (i = cwd_n; i-- != 0;) {
+        t_fileinfo *f = sorted[i];
+
+        if (f->type == directory && !dot_or_dotdot(f->name)) {
+            if (!dir_name || f->name[0] == '/')
+                queue_directory(f->name);
+            else {
+                char *name = file_name_concat(dir_name, f->name);
+                queue_directory(name);
+                free(name);
+            }
+        }
+    }
+}
+
+static void
+print_current_files(void)
+{
+    size_t i;
+
+    for (i = 0; i < cwd_n; i++) {
+        write(1, ((t_fileinfo **)sorted)[i]->name,
+                strlen(((t_fileinfo **)sorted)[i]->name));
+        write(1, "\n", 1);
+    }
+}
+
 void
 open_directory(const char *dir_name)
 {
@@ -45,23 +193,38 @@ open_directory(const char *dir_name)
     if (!dir) {
         write(2, "ft_ls: ", 7);
         perror(dir_name);
-        exit(1);
+        return ;
     }
-    while ((rdir = readdir(dir))) {
-        if (~mode & ALL && rdir->d_name[0] == '.')
-            continue ;
-        write(1, rdir->d_name, strlen(rdir->d_name));
-        write(1, "\n", 1);
-        if (mode & RECURSIVE && rdir->d_type & DT_DIR
-                && strcmp("..", rdir->d_name) < 0) {
-            char temp[4096] = {0};
-            strlcat(temp, dir_name, sizeof(temp));
-            strlcat(temp, "/", sizeof(temp));
-            strlcat(temp, rdir->d_name, sizeof(temp));
-            queue_directory(temp);
+
+    clear_file();
+
+    while (1) {
+        rdir = readdir(dir);
+        if (rdir) {
+            enum file_type type = unknown;
+
+            if (~mode & ALL && rdir->d_name[0] == '.')
+                continue ;
+            switch (rdir->d_type) {
+                case DT_BLK: type = blockdev; break;
+                case DT_CHR: type = chardev; break;
+                case DT_DIR: type = directory; break;
+                case DT_FIFO: type = fifo; break;
+                case DT_LNK: type = symbolic_link; break;
+                case DT_REG: type = normal; break;
+                case DT_SOCK: type = sock; break;
+            }
+            register_file(rdir->d_name, type);
         }
+        else
+            break;
     }
     closedir(dir);
+    sort_files();
+    if (mode & RECURSIVE)
+        extract_dir_from_files(dir_name);
+    if (cwd_n)
+        print_current_files();
 }
 
 static bool
@@ -98,9 +261,16 @@ main(int ac, char **av)
 
     if (ac > 1 && _getopt(av[1]))
         av = &av[1];
+
+    cwd_alloc = 100;
+    cwd = malloc(cwd_alloc * sizeof *cwd);
+    if (!cwd)
+        exit(3);
+    cwd_n = 0;
+
     open_directory(av[1] ? av[1] : ".");
-    pending_dir = pending ? pending : NULL;
-    while (pending_dir) {
+    while (pending) {
+        pending_dir = pending;
         write(1, "\n", 1);
         write(1, pending_dir->name, strlen(pending_dir->name));
         write(1, ":\n", 2);
@@ -108,6 +278,5 @@ main(int ac, char **av)
         open_directory(pending_dir->name);
         free(pending_dir->name);
         free(pending_dir);
-        pending_dir = pending;
     }
 }
