ba2ee629bd80c0261d562b8b3bf3bdacaf0453e0
sebamiro <seba.mi.21@gmail.com>
2024-02-14 21:19:40 +0100

    Refactoring

--- src/main.c
+++ src/main.c
@@ -1,469 +1,331 @@
 //
-// smiro
-// ft_ls	main.c
+// smiro ft_ls
+// main.c
 // 2024-02
 //
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/_types/_ssize_t.h>
-#include <unistd.h>
+#include "../lib/libft/libft.h"
+// #include "libft/libft.h"
+
+#include "defines.h"
+
 #include <stdbool.h>
-#include <string.h>
-#include <dirent.h>
+#include <stdio.h>
+#include <sys/dirent.h>
 #include <sys/stat.h>
-#include <pwd.h>
-#include <grp.h>
-#include <time.h>
-#include <sys/acl.h>
-#include <sys/xattr.h>
+#include <unistd.h>
+#include <dirent.h>
 
-#include "../inc/ft_ls.h"
+static bool get_opt(const char *opt);
+static void *xmalloc(size_t size);
+static char *xstrdup(const char *s);
+static int open_directroy(const char *dir_name);
 
-enum modes {
-    LIST = 0x01,
-    RECURSIVE = 0x02,
-    ALL = 0x04,
-    REVERSE = 0x08,
-    TIME = 0x10
-};
-
-#define USAGE write(2, "usage: ls [-lRart]\n", 20)
-
-enum file_type {
-    unknown,
-    blockdev,
-    chardev,
-    directory,
-    fifo,
-    symbolic_link,
-    normal,
-    sock
-};
-
-typedef struct s_pending
+int main(int ac, char **av)
 {
-    char *name;
-    struct s_pending *next;
-} t_pending;
+	int			error;
 
-typedef struct s_fileinfo
-{
-    char *name;
-    enum file_type type;
-    struct stat stat;
-    char acl;
-} t_fileinfo;
-
-static char flags = 0;
-t_pending *pending = NULL;
-t_fileinfo *cwd;
-void **sorted;
-size_t cwd_n;
-size_t cwd_alloc;
-size_t sorted_files_alloc;
-
-static void
-clear_file(void)
-{
-    size_t i;
-
-    for (i = 0; i < cwd_n; i++) {
-        t_fileinfo *f = sorted[i];
-        free(f->name);
-    }
-    cwd_n = 0;
-}
-
-static char*
-file_name_concat(const char *dirname, const char *name)
-{
-    size_t total;
-    size_t dir_len;
-	size_t name_len;
-	size_t i;
-    char *cat;
-
-	dir_len = ft_strlen(dirname);
-	name_len = ft_strlen(name);
-    total = dir_len + name_len + 2;
-    cat = xmalloc(total);
-	for (i = 0; i < dir_len; i++)
-		cat[i] = dirname[i];
-	cat[i++] = '/';
-	for (; i < total - 1; i++)
-		cat[i] = name[i - dir_len - 1];
-	cat[i] = 0;
-    return cat;
-}
-
-static void
-queue_directory(const char *name)
-{
-    t_pending *new = xmalloc(sizeof(t_pending));
-    new->name = ft_strdup(name);
-    if (!new->name)
-        exit(3);
-    new->next = pending;
-    pending = new;
-}
-
-static void
-initialize_sort(void)
-{
-    size_t i;
-
-    for (i = 0; i < cwd_n; i++) {
-        sorted[i] = &cwd[i];
-    }
-}
-
-static void
-sort_files(void)
-{
-    void *t;
-    size_t n;
-    size_t newn;
-
-    if (sorted_files_alloc < cwd_n + cwd_n / 2)
-    {
-        free(sorted);
-        sorted = xmalloc(cwd_n * 2 * sizeof *sorted);
-        sorted_files_alloc = 2 * cwd_n;
-    }
-    initialize_sort();
-    n = cwd_n;
-    while (n > 1) {
-        newn = 0;
-        for (size_t i = 1; i <= n - 1; i++) {
-            long long cmp = ft_strcmp(
-                        ((t_fileinfo **)sorted)[i - 1]->name,
-                        ((t_fileinfo **)sorted)[i]->name
-                        );
-			if (flags & TIME) {
-				cmp = ((t_fileinfo **)sorted)[i]->stat.st_mtimespec.tv_sec - ((t_fileinfo **)sorted)[i - 1]->stat.st_mtimespec.tv_sec;
-			}
-			if (flags & REVERSE) {
-				cmp *= -1;
-			}
-			if (cmp > 0) {
-               t = sorted[i - 1];
-               sorted[i - 1] = sorted[i];
-               sorted[i] = t;
-               newn = i;
-            }
-        }
-        n = newn;
-    }
-}
-
-static void
-print_ident_digit(int ident, int digit)
-{
-    static char str[5] = "%xd ";
-    str[1] = ident + 0x30;
-    ft_printf(str, digit);
-}
-
-static void
-print_byte_size(struct stat *s, bool print)
-{
-    static long long n = 10;
-    static int ident = 1;
-
-    if (!s) {
-        n = 10;
-        ident = 1;
-        return ;
-    }
-    if (s->st_size >= n) {
-        n *= 10;
-        ident += 1;
-        print_byte_size(s, false);
-    }
-    print ? print_ident_digit(ident, s->st_size) : 0;
-}
-
-static void
-print_n_link(struct stat *s, bool print)
-{
-	return ;
-    static long long n = 10;
-    static int ident = 1;
-
-    if (!s) {
-        n = 10;
-        ident = 1;
-        return ;
-    }
-    if (s->st_nlink >= n) {
-        n *= 10;
-        ident += 1;
-        print_byte_size(s, false);
-    }
-    print ? print_ident_digit(ident, s->st_nlink) : 0;
-}
-
-void
-has_acl(const char *absolute, t_fileinfo *f) {
-    ssize_t xattr;
-    acl_t acl = NULL;
-    acl_entry_t dummy;
-
-    xattr = listxattr(absolute, NULL, 0, XATTR_NOFOLLOW);
-    acl = acl_get_link_np(absolute, ACL_TYPE_EXTENDED);
-    if (acl && acl_get_entry(acl, ACL_FIRST_ENTRY, &dummy) == -1) {
-        acl_free(acl);
-        acl = NULL;
-    }
-    if (xattr > 0) {
-        f->acl = '@';
-    }
-    else if (acl) {
-        f->acl = '+';
+	(void)ac;
+	if (get_opt(*(++av))) {
+		av++;
 	}
-    else {
-        f->acl = ' ';
-	}
-    acl_free(acl);
+	cwd_alloc = 100;
+	cwd = xmalloc(cwd_alloc * sizeof *cwd);
+	cwd_n = 0;
+	error = 0;
+	return open_directroy(*av ? *av : ".");
 }
 
-static int
-register_file(const char *name, const char *dirname, enum file_type type)
+static void clear_files(void);
+static int open_pending(pending_t *);
+static file_type get_type(unsigned char);
+static size_t register_file(const char *, const char *, file_type);
+static void sort_files(void);
+static void extract_dir(const char *dir_name);
+static void print_current_files(void);
+
+static int open_directroy(const char *dir_name)
 {
-    t_fileinfo *f;
-    size_t block;
+	DIR	*dir;
+	size_t block;
+	struct dirent *rdir;
 
-    block = 0;
-    if (cwd_n == cwd_alloc) {
-        cwd = realloc(cwd,
-               2 * cwd_alloc * sizeof *cwd
-                );
-        cwd_alloc *= 2;
-    }
-    f = &cwd[cwd_n++];
-    f->type = type;
-    f->name = ft_strdup(name);
-    if (flags & TIME || flags & LIST) {
-        char *absolute;
-
-        if (name[0] == '/' || dirname[0] == 0) {
-            absolute = (char *)name;
-		} else {
-            absolute = file_name_concat(dirname, name);
+	if (!(dir = opendir(dir_name))) {
+		write(2, "ft_ls: ", 7);
+		perror(dir_name);
+		return 1;
+	}
+	block = 0;
+	clear_files();
+	while ((rdir = readdir(dir))) {
+		if (~flags & ALL && rdir->d_name[0] == '.') {
+			continue;
 		}
-        stat(absolute, &f->stat);
-        has_acl(absolute, f);
-        print_byte_size(&f->stat, false);
-        print_n_link(&f->stat, false);
-        block += f->stat.st_blocks;
-        free(absolute);
-    }
-    return block;
+		block += register_file(rdir->d_name, dir_name, get_type(rdir->d_type));
+	}
+	closedir(dir);
+	sort_files();
+	if (cwd_n) {
+		if (flags & RECURSIVE) {
+			extract_dir(dir_name);
+		}
+		if (flags & LIST) {
+			/* ft_printf("total %d\n", (int)block); */
+		}
+		print_current_files();
+	}
+	return open_pending(pending);
 }
 
-static bool
-dot_or_dotdot(const char *file_name)
+static void print_one_per_line(const char *file_name);
+
+static void print_current_files(void)
+{
+	size_t i;
+
+	for (i = 0; i < cwd_n; i++) {
+		if (flags & LIST) {
+			// TODO
+		} else {
+			print_one_per_line(((fileinfo_t **)sorted)[i]->name);
+		}
+	}
+}
+
+static void print_one_per_line(const char *file_name)
+{
+	write(1, file_name, ft_strlen(file_name));
+	write(1, "\n", 1);
+}
+
+static bool dot_or_dotdot(const char *);
+static void queue_directory(const char *name);
+static void file_name_concat(char *dest, const char *dir, const char *file);
+
+static void extract_dir(const char *dir_name)
+{
+	size_t i;
+	fileinfo_t *f;
+	char name[1024];
+
+	for (i = cwd_n; i-- > 0;) {
+		f = sorted[i];
+		if (f->type == directory && !dot_or_dotdot(f->name)) {
+			if (!dir_name || f->name[0] == '/') {
+				queue_directory(f->name);
+			} else {
+				file_name_concat(name, dir_name, f->name);
+				queue_directory(name);
+			}
+		}
+	}
+}
+
+static void queue_directory(const char *name)
+{
+	pending_t *n;
+
+	n = xmalloc(sizeof(pending_t));
+	n->name = xstrdup(name);
+	n->next = pending;
+	pending = n;
+}
+
+static bool dot_or_dotdot(const char *file_name)
 {
     if (file_name[0] == '.')
         return !file_name[(file_name[1] == '.') + 1];
     return false;
 }
 
-static void
-extract_dir_from_files(const char *dir_name)
+
+static void initialize_sort(void);
+
+static void sort_files(void)
 {
-    size_t i;
+	void *t;
+	size_t i, n, newn;
+	long long cmp;
 
-    for (i = cwd_n; i-- != 0;) {
-        t_fileinfo *f = sorted[i];
-
-        if (f->type == directory && !dot_or_dotdot(f->name)) {
-            if (!dir_name || f->name[0] == '/')
-                queue_directory(f->name);
-            else {
-                char *name = file_name_concat(dir_name, f->name);
-                queue_directory(name);
-                free(name);
-            }
-        }
-    }
-}
-
-static void
-print_owner(struct stat *s)
-{
-    char *owner;
-
-    owner = getpwuid(s->st_uid)->pw_name;
-    write(1, owner, ft_strlen(owner));
-    write(1, "  ", 2);
-}
-
-static void
-print_group(struct stat *s)
-{
-    char *group;
-
-    group = getgrgid(s->st_gid)->gr_name;
-    write(1, group, ft_strlen(group));
-    write(1, "  ", 2);
-}
-
-static void
-print_file_rights(t_fileinfo *f)
-{
-    char m[3] = "rwx";
-    int x = S_IRUSR;
-    for (int i = 0; i < 9; i++) {
-        if (f->stat.st_mode & x)
-            write(1, &m[i % 3], 1);
-        else
-            write(1, "-", 1);
-        x >>= 1;
-    }
-    write(1, &f->acl, 1);
-}
-
-static void
-print_long_format(t_fileinfo *f)
-{
-    write(1, f->type == directory ? "d" : "-", 1);
-    print_file_rights(f);
-    write(1, " ", 1);
-    print_n_link(&f->stat, true);
-    print_owner(&f->stat);
-    print_group(&f->stat);
-    print_byte_size(&f->stat, true);
-    write(1, ctime((time_t *)&f->stat.st_mtimespec) + 4, 12);
-    write(1, " ", 1);
-    write(1, f->name, ft_strlen(f->name));
-    write(1, "\n", 1);
-}
-
-static void
-print_one_per_line(t_fileinfo *f)
-{
-    write(1, f->name, ft_strlen(f->name));
-    write(1, "\n", 1);
-}
-
-static void
-print_current_files(void)
-{
-    size_t i;
-
-    for (i = 0; i < cwd_n; i++) {
-        if (flags & LIST)
-            print_long_format(((t_fileinfo **)sorted)[i]);
-        else
-            print_one_per_line(((t_fileinfo **)sorted)[i]);
-    }
-}
-
-int
-open_directory(const char *dir_name)
-{
-    DIR *dir;
-    struct dirent *rdir;
-	size_t block;
-
-    block = 0;
-    dir = opendir(dir_name);
-    if (!dir) {
-        write(2, "ft_ls: ", 7);
-        perror(dir_name);
-        return 1;
-    }
-
-    clear_file();
-    print_byte_size(NULL, false);
-    print_n_link(NULL, false);
-
-    while ((rdir = readdir(dir))) {
-		enum file_type type = unknown;
-
-		if (~flags & ALL && rdir->d_name[0] == '.')
-			continue ;
-		switch (rdir->d_type) {
-			case DT_BLK: type = blockdev; break;
-			case DT_CHR: type = chardev; break;
-			case DT_DIR: type = directory; break;
-			case DT_FIFO: type = fifo; break;
-			case DT_LNK: type = symbolic_link; break;
-			case DT_REG: type = normal; break;
-			case DT_SOCK: type = sock; break;
+	if (sorted_alloc < cwd_n) {
+		sorted_alloc = cwd_n * 2;
+		sorted = realloc(sorted, sorted_alloc * sizeof *sorted);
+		if (!sorted) {
+			exit(1);
 		}
-		block += register_file(rdir->d_name, dir_name, type);
-    }
-    closedir(dir);
-    sort_files();
-    if (flags & RECURSIVE)
-        extract_dir_from_files(dir_name);
-    if (cwd_n) {
-        if (flags & LIST) {
-            ft_printf("total %d\n", (int)block);
+	}
+	initialize_sort();
+	n = cwd_n;
+	while (n > 1) {
+		newn = 0;
+		for (i = 1; i < n; i++) {
+			if (flags & TIME) {
+				cmp = ((fileinfo_t **)sorted)[i]->stat.st_mtimespec.tv_sec
+					- ((fileinfo_t **)sorted)[i - 1]->stat.st_mtimespec.tv_sec;
+			} else {
+				cmp = ft_strcmp(((fileinfo_t **)sorted)[i - 1]->name,
+						((fileinfo_t **)sorted)[i]->name);
+			}
+			if (flags & REVERSE) {
+				cmp = -cmp;
+			}
+			if (cmp > 0) {
+				t = sorted[i - 1];
+				sorted[i - 1] = sorted[i];
+				sorted[i] = t;
+				newn = i;
+			}
 		}
-        print_current_files();
-    }
+		n = newn;
+	}
+}
+
+static void initialize_sort(void)
+{
+	size_t i;
+
+	for (i = 0; i < cwd_n; i++) {
+		sorted[i] = &cwd[i];
+	}
+}
+
+
+static size_t register_file(const char *name, const char *dirname, file_type t)
+{
+	fileinfo_t *f;
+
+	(void)dirname;
+	if (cwd_n == cwd_alloc) {
+		cwd_alloc = cwd_alloc * 2;
+		cwd = realloc(cwd, cwd_alloc * sizeof *cwd);
+		if (!cwd) {
+			exit(1);
+		}
+	}
+	f = &cwd[cwd_n++];
+	f->type = t;
+	f->name = xstrdup(name);
+	if (flags & TIME || flags & LIST) {
+		char absolute[1024];
+		file_name_concat(absolute, dirname, name);
+		stat(absolute, &f->stat);
+		// acl
+		// byte size
+		// n link
+		return f->stat.st_blocks;
+	}
 	return 0;
 }
 
-static bool _getopt(char *opt);
-
-int
-main(int ac, char **av)
+static void file_name_concat(char *dest, const char *dir, const char *file)
 {
-    t_pending *pending_dir;
+	size_t i, j;
+
+	i = 0;
+	while (dir[i]) {
+		dest[i] = dir[i];
+		i++;
+	}
+	dest[i++] = '/';
+	for (j = 0; file[j]; j++) {
+		dest[i++] = file[j];
+	}
+	dest[i] = '\0';
+}
+
+static file_type get_type(unsigned char t)
+{
+	switch (t) {
+		case DT_BLK: return blockdev;
+		case DT_CHR: return chardev;
+		case DT_DIR: return directory;
+		case DT_FIFO: return fifo;
+		case DT_LNK: return symbolic_link;
+		case DT_REG: return normal;
+		case DT_SOCK: return sock;
+	}
+	return unknown;
+}
+
+static void clear_files(void)
+{
+	size_t i;
+
+	for (i = 0; i < cwd_n; i++) {
+		fileinfo_t *f = sorted[i];
+		free(f->name);
+	}
+	cwd_n = 0;
+}
+
+static void print_dir_name(const char *name);
+
+static int open_pending(pending_t *pending_dir)
+{
+	pending_t	*next;
 	int error;
 
-	error = 0;
-    if (ac > 1 && _getopt(av[1]))
-        av = &av[1];
-
-    cwd_alloc = 100;
-    cwd = xmalloc(cwd_alloc * sizeof *cwd);
-    cwd_n = 0;
-
-    error = error || open_directory(av[1] ? av[1] : ".");
-    while (pending) {
-        pending_dir = pending;
-        write(1, "\n", 1);
-        write(1, pending_dir->name, ft_strlen(pending_dir->name));
-        write(1, ":\n", 2);
-        pending = pending->next;
-        error = error || open_directory(pending_dir->name);
-        free(pending_dir->name);
-        free(pending_dir);
-    }
-	return error;
+	if (!pending_dir) {
+		return 0;
+	}
+	error = open_directroy(pending_dir->name);
+	print_dir_name(pending_dir->name);
+	next = pending_dir->next;
+	free(pending_dir->name);
+	free(pending_dir);
+	return error || open_pending(next);
 }
 
-static bool
-_getopt(char *opt)
+static void print_dir_name(const char *name)
 {
-    if (opt[0] != '-')
-        return false;
-    for (int i = 1; opt[i]; i++) {
-        switch (opt[i]) {
-            case 'l':
-                flags |= LIST; break;
-            case 'R':
-                flags |= RECURSIVE; break;
-            case 'a':
-                flags |= ALL; break;
-            case 'r':
-                flags |= REVERSE; break;
-            case 't':
-                flags |= TIME; break;
-            default:
-                write(2, "ft_ls: illegal option -- ", 26);
-                write(2, &opt[i], 1);
-                write(2, "\n", 1);
-				USAGE;
-                exit(1);
-        }
-    }
-    return true;
+	write(1, "\n", 1);
+	write(1, name, ft_strlen(name));
+	write(1, ":\n", 2);
 }
 
+static bool get_opt(const char *opt)
+{
+	if (!opt || *(opt++) != '-') {
+		return false;
+	}
+	while (*opt) {
+		switch (*(opt++)) {
+			case 'l':
+				flags |= LIST; break;
+			case 'R':
+				flags |= RECURSIVE; break;
+			case 'a':
+				flags |= ALL; break;
+			case 'r':
+				flags |= REVERSE; break;
+			case 't':
+				flags |= TIME; break;
+			default:
+				write(2, "ft_ls: illegal option -- ", 26);
+				write(2, opt, 1);
+				USAGE;
+				exit(1);
+		}
+	}
+	return true;
+}
+
+static void *xmalloc(size_t size)
+{
+	void *p;
+
+	if (!(p = malloc(size))) {
+		exit(1);
+	}
+	return p;
+}
+
+static char *xstrdup(const char *s)
+{
+	char *p;
+
+	if (!(p = ft_strdup(s))) {
+		exit(1);
+	}
+	return p;
+}
